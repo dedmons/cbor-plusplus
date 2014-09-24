96b65a8f0515ad14095f774ac027caec3454e19a
Douglas Edmonson <djedmonson@gmail.com>
2014-09-23 21:45:26 -0400

    Reworking the value class

--- include/cbor++/CBORValue.h
+++ include/cbor++/CBORValue.h
@@ -4,13 +4,52 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include <string.h>
+#include <vector.h>
+#include <map.h>
+
+enum CBORType {
+  CBORType_PInt,
+  CBORType_NInt,
+  CBORType_BString,
+  CBORType_UString,
+  CBORType_Array,
+  CBORType_Map,
+}
+
+typedef std::vector<CBORValue> CBORArray
+typedef std::map<CBORValue,CBORValue> CBORMap
+
 class CBORValue {
   public:
-    size_t getSize() const { return size; };
-    virtual size_t getBytes(uint8_t * const bytes) const = 0;
-    virtual const void * getValue() const = 0;
-  protected:
-    size_t size;
+    CBORValue(const uint8_t * bytes);
+
+    CBORValue(const uint64_t tag, const uint64_t pInt);
+    CBORValue(const uint64_t tag, const int64_t nInt);
+    CBORValue(const uint64_t tag, const char * bStr);
+    CBORValue(const uint64_t tag, const std::string * uStr);
+    CBORValue(const uint64_t tag, const CBORArray * arr);
+    CBORValue(const uint64_t tag, const CBORMap * map);
+
+    bool isType(CBORType type) const;
+    bool isPosInteger() const;
+    bool isNegInteger() const;
+    bool isByteString() const;
+    bool isUTF8String() const;
+    bool isAarray() const;
+    bool isMap() const;
+
+    const uint8_t * getBytes() const;
+
+    uint64_t getPosInteger() const;
+    int64_t getNegInteger() const;
+    const char * getByteString() const;
+    const std::string getUTF8String() const;
+    const CBORArray getAarray() const;
+    const CBORMap getMap() const;
+
+  private:
+    CBORType type;
 };
 
 #endif /* CBOR_VALUE__H */
