2cf0c8c0f34b139d4834139c50cedfbf218849c7
Douglas Edmonson <djedmonson@gmail.com>
2014-09-29 21:15:13 -0400

    Added std prefix to int types

--- include/cbor++/CBORValue.h
+++ include/cbor++/CBORValue.h
@@ -30,7 +30,7 @@ class CBORValue {
     CBORValue(const std::uint64_t tag, const std::uint64_t intVal, const bool isNeg);
     CBORValue(const std::uint64_t tag, const std::string * str, const bool isUTF8);
     CBORValue(const std::uint64_t tag, const CBORArray * arr);
-    CBORValue(const uint64_t tag, const CBORMap * map);
+    CBORValue(const std::uint64_t tag, const CBORMap * map);
 
     bool isType(CBORType type) const;
     bool isPosInteger() const;
@@ -40,10 +40,10 @@ class CBORValue {
     bool isAarray() const;
     bool isMap() const;
 
-    const uint8_t * getBytes() const;
+    const std::uint8_t * getBytes() const;
 
-    uint64_t          getPosInteger() const;
-    int64_t           getNegInteger() const;
+    std::uint64_t     getPosInteger() const;
+    std::int64_t      getNegInteger() const;
     const std::string getByteString() const;
     const std::string getUTF8String() const;
     const CBORArray   getAarray() const;
@@ -52,11 +52,11 @@ class CBORValue {
   private:
     CBORType    type;
 
-    bool        isNegative;
-    uint64_t    integerValue;
-    std::string stringValue;
-    CBORArray   arrayValue;
-    CBORMap     mapValue;
+    bool          isNegative;
+    std::uint64_t integerValue;
+    std::string   stringValue;
+    CBORArray     arrayValue;
+    CBORMap       mapValue;
 };
 
 #endif /* CBOR_VALUE__H */
