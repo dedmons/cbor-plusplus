4d3207524ecf566ab1ff32fd21bfa9b3ab1ba4f0
Douglas Edmonson <douglasedmonson732@air-watch.com>
2014-10-01 09:01:00 -0400

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -2,17 +2,17 @@
 #include <cbor++/version.h>
 #include "libraryinfo.h"
 
-EXTERNC int projectname_version_major()
+EXTERNC int cborplusplus_version_major()
 {
     return LIBRARY_VERSION_MAJOR;
 }
 
-EXTERNC int projectname_version_minor()
+EXTERNC int cborplusplus_version_minor()
 {
     return LIBRARY_VERSION_MINOR;
 }
 
-EXTERNC int projectname_version_patch()
+EXTERNC int cborplusplus_version_patch()
 {
     return LIBRARY_VERSION_PATCH;
 }
